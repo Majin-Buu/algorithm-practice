a815c2625a5e34315a9fb5a6a1fc08e8abb9e10a
tcsl-lsy-zhuliangshuang <FatMaJin_Buu@126.com>
2020-09-28 10:54:22 +0800

    add: 末尾加一

--- src/com/company/ArrayAlgorithm.java
+++ src/com/company/ArrayAlgorithm.java
@@ -35,6 +35,8 @@ public class ArrayAlgorithm {
         System.out.println(Arrays.toString(i));
         // 两个数组的交集
         System.out.println(Arrays.toString(intersect(new int[]{1, 2, 3}, new int[]{1, 2, 3})));
+        // 数组末尾加一，并且将位数大的往前放
+        System.out.println(Arrays.toString(plusOne(new int[]{9, 9, 9})));
 
     }
 
@@ -302,6 +304,33 @@ public class ArrayAlgorithm {
         }
 
         return list.stream().mapToInt(Integer::valueOf).toArray();
+    }
+
+    /**
+     * 给定一个由整数组成的非空数组所表示的非负整数，在该数的基础上加一。
+     * 最高位数字存放在数组的首位， 数组中每个元素只存储单个数字。
+     * 你可以假设除了整数 0 之外，这个整数不会以零开头。
+     * 示例 1:
+     * 输入: [1,2,3]
+     * 输出: [1,2,4]
+     * 解释: 输入数组表示数字 123。
+     * 示例 2:
+     * 输入: [4,3,2,1]
+     * 输出: [4,3,2,2]
+     * 解释: 输入数组表示数字 4321。
+     * 这个算法是真牛逼
+     */
+    public static int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            digits[i]++;
+            digits[i] = digits[i] % 10;
+            if (digits[i] != 0) {
+                return digits;
+            }
+        }
+        digits = new int[digits.length + 1];
+        digits[0] = 1;
+        return digits;
     }
 
 }
--- src/com/company/StringAlgorithm.java
+++ src/com/company/StringAlgorithm.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Arrays;
+
 /**
  * @author MaJin_Buu
  */
@@ -9,6 +11,20 @@ public class StringAlgorithm {
         char[] str = new char[]{'s', 'b', 'g', 't'};
         reverseString(str);
         System.out.println(str);
+
+        String[] str1 = {"J", "a", "v", "a", "中"};
+        String[] str2 = {"如", "何", "把", "两", "个", "数", "组", "合", "并", "为", "一", "个"};
+        int strLen1 = str1.length;//保存第一个数组长度
+        int strLen2 = str2.length;//保存第二个数组长度
+        str1 = Arrays.copyOf(str1, strLen1 + strLen2);//扩容
+        System.arraycopy(str2, 0, str1, strLen1, strLen2);//将第二个数组与第一个数组合并
+        System.out.println(Arrays.toString(str1));//输出数组
+
+        int[] digits = {1, 2, 3};
+        int[] digitMin = {1, 2, 3, 4};
+
+        digitMin = Arrays.copyOf(digitMin, digits.length + digitMin.length);
+        System.arraycopy(digits, 0, digitMin, digitMin.length, digits.length);
 
     }
 
